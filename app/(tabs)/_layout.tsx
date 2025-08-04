672268e03eba8d4497a286f771d34fac30622210
AdhamHaisami <adham9633@gmail.com>
2025-08-04 17:48:08 +0300

    Initial commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,49 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
-
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Tabs } from "expo-router";
+export default function TabsLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
+    <Tabs screenOptions={{
+      headerStyle: { backgroundColor: "#f5f5f5" },
+      headerShadowVisible: false,
+      tabBarStyle: {
+        backgroundColor: "#f5f5f5",
+        borderTopWidth: 0,
+        elevation: 0,
+        shadowOpacity: 0,
+      },
+      tabBarActiveTintColor: "#6200ee",
+      tabBarInactiveTintColor: "#666666",
+    }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
+        title: "Today's habits",
+        headerTitleAlign: 'center',
+        tabBarIcon: ({ color, size }) => (
+          // <FontAwesome5 name='home' size={24} color={color} />
+          <MaterialCommunityIcons name='calendar-today' color={color} size={size}/>
+          )
+        }} />
       <Tabs.Screen
-        name="explore"
+        name="streaks"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+        title: "Streaks",
+        headerTitleAlign: 'center',
+        tabBarIcon: ({ color, size }) => (
+          // <FontAwesome5 name='home' size={24} color={color} />
+          <MaterialCommunityIcons name='chart-line' color={color} size={size}/>
+          )
+        }} />
+       <Tabs.Screen
+        name="add-habit"
+        options={{
+        title: "Add Habit",
+        headerTitleAlign: 'center',
+        tabBarIcon: ({ color, size }) => (
+          // <FontAwesome5 name='home' size={24} color={color} />
+          <MaterialCommunityIcons name='plus-circle' color={color} size={size}/>
+      )}} />
+      <Tabs.Screen name="login" options={{title: 'Login'}}/>
     </Tabs>
   );
 }
